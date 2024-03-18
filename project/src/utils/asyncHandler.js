5b68744a82e22cea9b105dfbc0b6b992044746a7
Inam Ilyas <inaamilyas656@gmail.com>
2024-03-18 21:34:04 +0500

    Writing utils and api errors handling and setting up standard

---
+++ project/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHanlder = (requestHandler)=>{
+    (req, res, next)=>{
+        Promise.resolve((requestHandler(req, res, next)))
+        .catch((err) => next(err))
+    }
+}
+
+
+export {asyncHanlder}
+
+
+
+// const asyncHanlder = (fn)=>{
+//     async (req, res, next)=>{
+//         try {
+//             await fn(req, res, next);
+//         } catch (error) {
+//             res.status(error.code || 500).json({
+//                 success:false,
+//                 message:error.message
+//             })
+//         }
+//     }
+// }
