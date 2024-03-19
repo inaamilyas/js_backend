5ab75411c8be4d3523bad12ea9a58f90d4de4d27
Inam Ilyas <inaamilyas656@gmail.com>
2024-03-19 22:54:04 +0500

    complete project setup

--- project/src/index.js
+++ project/src/index.js
@@ -1,26 +1,20 @@
 // require('.dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
-
+import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
-
+  path: "./env",
+});
 connectDb()
-.then(()=>{
-    app.listen(process.env.PORT || 800, ()=>{
-        console.log("Server is running on port " + process.env.PORT);
-    })
-})
-.catch(err=>{
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running on port " + process.env.PORT);
+    });
+  })
+  .catch((err) => {
     console.log("Mongo db connection failed !!! ", err);
-})
-
-
-
-
-
+  });
 
 /*
 First Approach for connecting to db
--- project/src/utils/asyncHandler.js
+++ project/src/utils/asyncHandler.js
@@ -1,14 +1,10 @@
-const asyncHanlder = (requestHandler)=>{
-    (req, res, next)=>{
-        Promise.resolve((requestHandler(req, res, next)))
-        .catch((err) => next(err))
-    }
-}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-
-export {asyncHanlder}
-
-
+export { asyncHandler };
 
 // const asyncHanlder = (fn)=>{
 //     async (req, res, next)=>{
